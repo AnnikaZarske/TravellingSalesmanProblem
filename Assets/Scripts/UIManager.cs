5a793bf7deb696fcc9db8a0c91c5afda4ea6c8a0
DESKTOP-2COJ1V1\Annika <annikazarske49@gmail.com>
2022-01-04 09:46:05 +0100

    First commit + basic layout made

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public string menuScene;
+
+    public void OnBackClick()
+    {
+        SceneManager.LoadScene(menuScene);
+    }
+}
